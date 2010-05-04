5a87586c155f259ca148a6083bd3c96c596946f0
Olivier Ramonat <olivier@ramonat.fr>
2010-05-04 21:30:11 +0200

    Add setup.py

--- prenestr/server.py
+++ prenestr/server.py
@@ -160,25 +160,3 @@ class Prenestr(Daemon):
                         getattr(x, command)()
                     else:
                         debug("%s command not found" % command)
-
-
-def init():
-    daemon = Prenestr("/tmp/prenestr.pid")
-    if len(sys.argv) == 2:
-        if 'start' == sys.argv[1]:
-            daemon.start()
-        elif 'stop' == sys.argv[1]:
-            daemon.stop()
-        elif 'restart' == sys.argv[1]:
-            daemon.restart()
-        else:
-            print "Unknown command"
-            sys.exit(2)
-        sys.exit(0)
-    else:
-        print "usage: %s start|stop|restart" % sys.argv[0]
-        sys.exit(2)
-
-
-if __name__ == '__main__':
-    init()
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="prenestr",
+      version="1.0",
+      author="Olivier Ramonat",
+      author_email="olivier@ramonat.fr",
+      packages=["prenestr"],
+      scripts=["prenestr-send", "prenestr-daemon"])
